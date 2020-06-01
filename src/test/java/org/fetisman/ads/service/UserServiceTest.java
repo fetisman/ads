1f0af57cec364f339bae45a51b4f81c77265ba60
fetisman <fetismanjava@gmail.com>
2020-06-01 09:04:45 +0300

    Add User service test

---
+++ src/test/java/org/fetisman/ads/service/UserServiceTest.java
@@ -0,0 +1,106 @@
+package org.fetisman.ads.service;
+
+import java.util.Collections;
+import org.fetisman.ads.domain.Role;
+import org.fetisman.ads.domain.User;
+import org.fetisman.ads.repo.UserRepo;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @MockBean
+    private UserRepo userRepo;
+
+    @MockBean
+    private MailSender mailSender;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void addUser() {
+        User user = new User();
+        user.setEmail("some@somemail.come");
+        boolean isUserCreated = userService.addUser(user);
+        Assert.assertTrue(isUserCreated);
+
+        Assert.assertNotNull(user.getActivationCode());
+
+        Assert.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.USER)));
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+
+        Mockito.verify(mailSender, Mockito.times(1))
+                .send(
+                        ArgumentMatchers.eq(user.getEmail()),
+                        ArgumentMatchers.eq("Activation code"),
+                        ArgumentMatchers.contains("Welcome to Ads")
+                );
+    }
+
+    @Test
+    public void addUserFail(){
+        User user = new User();
+        user.setUsername("ff103");
+
+        Mockito.doReturn(new User())
+                .when(userRepo)
+                .findByUsername("ff103");
+
+        boolean isUserCreated = userService.addUser(user);
+        Assert.assertFalse(isUserCreated);
+
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+
+        Mockito.verify(mailSender, Mockito.times(0))
+                .send(
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString()
+                );
+
+
+    }
+
+    @Test
+    public void activateUser() {
+
+        User user = new User();
+        user.setActivationCode("bingo");
+
+        Mockito.doReturn(user)
+                .when(userRepo)
+                .findByActivationCode("activate");
+
+        boolean isUserActivated = userService.activateUser("activate");
+
+        Assert.assertTrue(isUserActivated);
+        Assert.assertNull(user.getActivationCode());
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+
+    }
+
+    @Test
+    public void activateUserFail(){
+        boolean isUserActivated = userService.activateUser("activate me");
+
+        Assert.assertFalse(isUserActivated);
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+
+    }
+}
